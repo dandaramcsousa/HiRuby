f225a1e2898a50fcdf528bb8a4e7bbcc9cf73f4b
Dandara Sousa <dandaramcsousa@gmail.com>
2019-08-28 15:31:53 -0300

    exercicio calculadora

---
+++ codeSaga01.rb
@@ -0,0 +1,41 @@
+#  codeSaga01.rb
+#  
+#  Copyright 2019 Dandara <Dandara@DESKTOP-EE899N5>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+class StdClass
+	def initialize
+		
+	end
+end
+
+x = StdClass.new
+
+print "algo" 
+puts "algo" #print + quebra de linha
+puts "---------------"
+nome = "Dandara"
+idade = 22
+cidade = "Campina Grande"
+
+puts "Meu nome é #{nome}."
+puts "Tenho #{idade} anos."
+puts "Moro em #{cidade}."
+print "Adeus..."
